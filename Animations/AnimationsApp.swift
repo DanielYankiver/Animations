4a93b2a7a0958996e6434a7a2e3e5efa8eb0323b
Daniel Yankiver <dyankiver@gmail.com>
2023-12-19 15:06:00 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Daniel Yankiver on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Daniel Yankiver on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
